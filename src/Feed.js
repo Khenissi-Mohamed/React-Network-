579977bbf6c5e2f9900bbe23acf2c72fa5cd9a58
Khenissi <mkhenissi@ymail.com>
2023-02-25 18:12:24 +0100

    2ème façon d 'empty posts

--- src/Feed.js
+++ src/Feed.js
@@ -30,20 +30,22 @@ function Feed() {
         console.log("posts", posts);
       }
 
-      const contentPosts = <>
-      <h3>Fil d'actualité :</h3>
+
+    return posts.length > 0 ? 
+    (
+        <>
+        <h3>Fil d'actualité :</h3>
         {posts.map(p => <Post key={p.id} postData={p} deletePost={deletePost} />)}
-      </>
-
-      const contentEmpty = <>
-      <h3 align="center">
-            {/* <img src={emptyImage} width='300' /> */}
-            <EmptyImageSvg width="300" height="300" />
-            Aucun post pour le moment
+        </>
+    ) : (
+        <>
+        <h3 align="center">
+        {/* <img src={emptyImage} width='300' /> */}
+        <EmptyImageSvg width="300" height="300" />
+        Aucun post pour le moment
         </h3>
-      </>
-
-    return posts.length > 0 ? contentPosts : contentEmpty
+        </>
+    )
 }
 
 export default Feed
